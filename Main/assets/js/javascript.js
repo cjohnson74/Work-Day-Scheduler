2a04b1b9f76e8b11c91f32f3db1480d3d45aab11
Carson Johnson <carson74johnson@gmail.com>
2021-10-02 15:49:57 -0400

    deleted unnecessary comments

--- Main/assets/js/javascript.js
+++ Main/assets/js/javascript.js
@@ -1,14 +1,10 @@
 $(document).ready(function () {
-  //Everything else goes here
   $(".saveBtn").on("click", function () {
     console.log("test");
     var value = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
     localStorage.setItem(time, value);
-
-    // console.log(value);
-    // console.log(time);
   });
 
   var currentHour = moment().hours();
@@ -26,21 +22,15 @@ $(document).ready(function () {
     } else if (j == currentTimeBlock) {
       currentBlockChild.append(todo);
     }
-    j++
+    j++;
   }
 
   var currentDay = moment().format("dddd, MMMM Do");
   $("#currentDay").append(currentDay);
 
-  // var time = new Date();
-  // var currentHour = time.getHours()
-
   for (var i = 0; i < timeBlocks.length; i++) {
     var currentTimeBlock = timeBlocks[i].getAttribute("id");
     var currentBlockChild = timeBlocks[i].childNodes[3];
-    // var value = currentBlockChild.val();
-    // var value = localStorage.getItem(currentTimeBlock);
-    // currentBlockChild.attr("text", value);
     if (currentTimeBlock < currentHour) {
       currentBlockChild.setAttribute("class", "col-md-10 past description");
     } else if (currentTimeBlock == currentHour) {
@@ -48,6 +38,5 @@ $(document).ready(function () {
     } else {
       currentBlockChild.setAttribute("class", "col-md-10 future description");
     }
-    // currentTimeBlock.children[1].setAttribute("class", "past");
   }
 });
